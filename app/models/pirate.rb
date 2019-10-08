cc0ef543b6d9770b35ba1f3aeaa155e68e773708
swdecker <swdecker@gmail.com>
2019-10-07 19:59:26 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,13 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+    @@all = []
+    def initialize(i_hash)
+        @name = i_hash[:name]
+        @weight = i_hash[:weight]
+        @height = i_hash[:height]
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
 end
